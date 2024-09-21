f6059154eaff145ff41a603227919f699e408992
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-09-21 18:53:08 -0300

    Antes do teste

--- Exercício_03/demo4/src/main/java/dao/DAO.java
+++ Exercício_03/demo4/src/main/java/dao/DAO.java
@@ -4,19 +4,19 @@ import java.sql.*;
 
 public class DAO {
 	protected Connection conexao;
-	
+
 	public DAO() {
 		conexao = null;
 	}
-	
+
 	public boolean conectar() {
-		String driverName = "org.postgresql.Driver";                    
+		String driverName = "org.postgresql.Driver";
 		String serverName = "localhost";
-		String mydatabase = "teste";
+		String mydatabase = "Teste";
 		int porta = 5432;
-		String url = "jdbc:postgresql://" + serverName + ":" + porta +"/" + mydatabase;
-		String username = "ti2cc";
-		String password = "ti@cc";
+		String url = "jdbc:postgresql://" + serverName + ":" + porta + "/" + mydatabase;
+		String username = "postgres";
+		String password = "Rimagajupe@4";
 		boolean status = false;
 
 		try {
@@ -24,7 +24,7 @@ public class DAO {
 			conexao = DriverManager.getConnection(url, username, password);
 			status = (conexao == null);
 			System.out.println("Conexão efetuada com o postgres!");
-		} catch (ClassNotFoundException e) { 
+		} catch (ClassNotFoundException e) {
 			System.err.println("Conexão NÃO efetuada com o postgres -- Driver não encontrado -- " + e.getMessage());
 		} catch (SQLException e) {
 			System.err.println("Conexão NÃO efetuada com o postgres -- " + e.getMessage());
@@ -32,10 +32,10 @@ public class DAO {
 
 		return status;
 	}
-	
+
 	public boolean close() {
 		boolean status = false;
-		
+
 		try {
 			conexao.close();
 			status = true;
